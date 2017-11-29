32f965437344321f7d748182be0f46ba90eacd01
Hedda Hognedatter Bjørnebye Vik <HeddaVik@tsf-460-wpa-6-196.epfl.ch>
2017-11-29 14:17:31 +0100

    fått n-grams til å virke riktig tror jeg

--- helpers.py
+++ helpers.py
@@ -39,29 +39,113 @@ def create_labels(nr_pos_lines,nr_neg_lines,kaggle=False):
     else:
         labels[nr_pos_lines:nr_lines_total]=0;
     return labels
-
-
-def add_n_grams(line, n):
+"""
+def add_n_grams(line, n_gram):
 
     n_grams = []
+    temp=[]
     words = line.split()
-    n_grams.append(b''.join([b'_',b'-',words[0]]))
-    for i in range(len(words)-1):        
-        n_grams.append(b''.join([words[i], b'-', words[i+1]]))
-     
-    n_grams.append(b''.join([words[-1], b'-', b"_"]))
+    #n_grams.append(b''.join([b'_',b'-',words[0]]))
+    for i in range(len(words)-(n_gram)): 
+        temp=[]
+        index=i
+        if n_gram is not 1:
+            for j in range(n_gram):
+                temp.append(words[index])
+                if j is not (n_gram-1):
+                    temp.append(b'-')
+                index+=1
+            n_grams.append(b''.join(temp))
+        else: 
+            n_grams.append(b''.join([words[i]]))
+    #n_grams.append(b''.join([words[-1], b'-', b"_"]))
+    #n_grams.append(b''.join([words[-1]]))
     return b' '.join(n_grams)
 
 def creating_n_grams_cropus(n_gram, corpus):
     new_corpus=[]
+    
     for line in corpus:
+        lines=[]
         if line:
-            line = add_n_grams(line, n_gram)
-        new_corpus.append(line)
+            for i in range(n_gram):
+                new_line = add_n_grams(line, i+1)
+                lines.append(new_line)
+        new_corpus.append(b' '.join(lines))
     
     return new_corpus
 
+"""
+def add_n_grams(line, n_gram):
 
+    n_grams = []
+    words = line.split()
+    if n_gram is not 1:
+        #need to treat the first n_gram-1 words diffrently:
+        for first_words in range(n_gram-1):
+            temp=[]
+            number_of_blanks= n_gram-(first_words+1)
+            for blank in range(number_of_blanks):
+                temp.append(b'_-')
+            words_to_fill_in=first_words+1
+            for w in range(words_to_fill_in):
+                temp.append(words[w])
+                if w is not (words_to_fill_in-1):
+                                temp.append(b'-')
+            n_grams.append(b''.join(temp))
+    
+    
+    for i in range(len(words)-(n_gram)+1): 
+        #creating empty temporary list
+        temp=[]
+        #initializing index:
+        index=i
+        if n_gram is not 1:    
+            #want to combine n_gram words in temp, with a '-' between each word.
+            for j in range(n_gram):
+                temp.append(words[index])
+                if j is not (n_gram-1):
+                    temp.append(b'-')
+                index+=1
+            #want to combine everything as a bit string.
+            n_grams.append(b''.join(temp))
+        else: 
+            n_grams.append(b''.join([words[i]]))
+            
+    if n_gram is not 1:
+        #need to treat the final n_gram-1 words diffrently:
+        for last_words in range(n_gram-1):
+            temp=[]
+            number_of_blanks= n_gram-(last_words+1)
+            words_to_fill_in=last_words+1
+            
+   
+            for w in range(words_to_fill_in,0,-1):
+                temp.append(words[-(w)])
+                temp.append(b'-')
+                        
+            for blank in range(number_of_blanks):
+                temp.append(b'_')
+                if blank is not number_of_blanks-1:
+                    temp.append(b'-')
+            
+            n_grams.append(b''.join(temp))
+
+    return b' '.join(n_grams)
+
+def creating_n_grams_cropus(n_gram, corpus):
+    new_corpus=[]
+    
+    for line in corpus:
+        lines=[]
+        if line:
+            for i in range(n_gram):
+                if len(line.split())>n_gram:
+                    new_line = add_n_grams(line, i+1)
+                    lines.append(new_line)
+        new_corpus.append(b' '.join(lines))
+    
+    return new_corpus
 
 def create_csv_submission(ids, y_pred, name):
     """
