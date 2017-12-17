fa5d29db333f0852367486d5a176c6d803ba20be
hbjornoy <havard.bjornoy@gmail.com>
2017-12-17 20:54:12 +0100

    shuffle and split in glove and pickle for MAC in helpers

--- glove_module.py
+++ glove_module.py
@@ -24,6 +24,7 @@ import helpers as HL
 
 from numpy.random import seed
 
+
 def create_gensim_word2vec_file(path_to_original_glove_file):
     """
     :param path_to_glove_folder: Go to Stanfords website https://nlp.stanford.edu/projects/glove/ and download their twitterdataset,
@@ -54,7 +55,6 @@ def get_dim_of_file(filename):
 
 
 def make_glove(path_to_gensim_global_vectors):
-    
     """
     uses the created gensim-.txt file to create the word2vec so one can operate on
 
@@ -63,7 +63,6 @@ def make_glove(path_to_gensim_global_vectors):
     :return: global vectors in the form of a list of words, with the words represented as vectors
             with length the same as the dimensions of the space
     """
-    
     glove_model = gensim.models.KeyedVectors.load_word2vec_format(path_to_gensim_global_vectors, binary=False)
 
     global_vectors = glove_model.wv
@@ -135,16 +134,19 @@ def run_k_fold(models, X, Y, epochs, n_folds):
     
     seed(1337)
     
-    #split_size = 30000
+    s = np.arange(X.shape[0])
     
-    #shuffle_indexes = np.arange(X.shape[0])
-    #np.random.shuffle(shuffle_indexes)
+    X = X[s]
+    Y = Y[s]
     
-    #X = X[shuffle_indexes]
-    #Y = Y[shuffle_indexes]
-    
-    #train_x, unseen_x = X[split_size:], X[:split_size]
-    #train_y, unseen_y = Y[split_size:], Y[:split_size]
+    #unseen_x = X[:10000]
+    #unseen_y = Y[:10000]
+
+    x_test = X[:40000]
+    y_test = Y[:40000]
+   
+    X = X[40000:]
+    Y = Y[40000:] 
     
     model_scores = []
     for neural_model in models:
@@ -157,39 +159,30 @@ def run_k_fold(models, X, Y, epochs, n_folds):
 
         kfold = sk.model_selection.StratifiedKFold(n_splits=n_folds)
         cv_scores = []
-       
         pos_scores = []
         neg_scores = []
         ratio_of_pos_guesses = []
-    
-        #unseen_scores = []
-    
+       
         for train, test in kfold.split(X, Y):
             
-            early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=4, verbose=1)
+            early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, verbose=1)
             
             model_checkpoint = keras.callbacks.ModelCheckpoint("best_neural_model_save.hdf5", monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False, mode='auto')
             
             model = neural_model(input_dimensions)
             
-            history = model.fit(X[train], Y[train], epochs=epochs, batch_size=1024, verbose=1, callbacks=[early_stopping, model_checkpoint], validation_data=(X[test], Y[test]))
+            history = model.fit(X[train], Y[train], epochs=epochs, batch_size=1024, verbose=1, callbacks=[early_stopping, model_checkpoint], validation_data=(x_test, y_test))
             
             model = load_model('best_neural_model_save.hdf5')
             
-            #Scores
+            score = model.evaluate(X[test], Y[test], verbose=1)[1]
             
-            cv_score = model.evaluate(X[test], Y[test], verbose=1)[1]
-            cv_scores.append(cv_score)
-            
-            #unseen_score = model.evaluate(unseen_x, unseen_y, verbose=1)[1]
-            #unseen_scores.append(unseen_score)
-            
-            print("CV-score:", cv_score)
-            #print("Unseen:", unseen_score)
+            print("Unseen score:", score)
 
-            #Predic for balance checking
             pred = model.predict(X[test])
-             
+            
+            cv_scores.append(score)
+ 
             # To analyze if it is unbalanced classifying
             labels = Y[test]
             pos_right = 0
@@ -204,10 +197,9 @@ def run_k_fold(models, X, Y, epochs, n_folds):
             neg_scores.append((neg_right / (len(labels) * 0.5))*100)
  
         print("Model: ", model_name)
-        print("CV_SCORES:", cv_scores)
-        print("CV_SCORES MEAN/STD:",  "%.2f%% (+/- %.2f%%)" % (np.mean(cv_scores), np.std(cv_scores)))
-        #print("UNSEEN_SCORES MEAN/STD:",  "%.2f%% (+/- %.2f%%)" % (np.mean(unseen_scores), np.std(unseen_scores)))
-        
+        print(history)
+        print(cv_scores)
+        print("%.2f%% (+/- %.2f%%)" % (np.mean(cv_scores), np.std(cv_scores)))
         print("Negative sentiment: %.2f%%  Positive sentiment: %.2f%%" % (np.mean(neg_scores), np.mean(pos_scores)))
         print("Percentage of positive classifications (should be 50%ish):", np.mean(ratio_of_pos_guesses)*100)
         print("Time taken: ", (time.time() - start) / 60, "\n")
@@ -240,10 +232,8 @@ def crossvalidation_for_dd(tuned_model, X, Y, epochs, n_folds):
         model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
         history = model.fit(X[train], Y[train], epochs=epochs, batch_size=1024, verbose=1, callbacks=[early_stopping, model_checkpoint], validation_data=(X[test], Y[test]))
-        
         score = model.evaluate(X[test], Y[test], verbose=0)
         cv_scores.append(score) # end results of the cv
-        
         histories.append(history)
         
     print("Val_accuracies: %.2f%% (+/- %.4f)" % (np.mean([cv_score[1]*100 for cv_score in cv_scores]), 
@@ -282,25 +272,28 @@ def testing_for_dd(tuned_model, X, Y, epochs, n_folds, split=0.9):
     return model, cv_histories, histories
 
 def train_NN(model, allX, allY, epochs):
-    split_size = int(allX.shape[0]*0.90)
     
-    train_x, unseen_x = allX[:split_size], allX[split_size:]
-    train_y, unseen_y = allY[:split_size], allY[split_size:]
+    train_document_vecs, labels = shuffle_data(allX,allY)
+    train_x, val_x, train_y, val_y = split_data(train_document_vecs, labels, split=0.80)
     
-    early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
+    early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, verbose=1)
+    model_checkpoint = keras.callbacks.ModelCheckpoint("DHA_BEST_dd_model.hdf5", monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False, mode='auto')
+    
+    histories = []
     start = time.time()
     try:
         while True:
-            prev_history = model.fit(train_x, train_y, epochs=epochs, batch_size=1024, verbose=1, callbacks=[early_stopping], 
-                                     validation_data=(unseen_x, unseen_y))
+            prev_history = model.fit(train_x, train_y, epochs=epochs, batch_size=5000, verbose=1, callbacks=[early_stopping, model_checkpoint], validation_data=(val_x, val_y))
             prev_model = model
+            histories.append(prev_history) 
         
     
     except (KeyboardInterrupt, SystemExit):
+        print('\n\ntime spent training:', (time.time() - start))
         return prev_model, prev_history
         #raise
     else:
-        print("why did this happen?")
+        print("\n\nwhy did this happen?")
         print(prev_model, prev_history)
         return prev_model, prev_history
 
@@ -348,73 +341,78 @@ def classify_with_neural_networks(neural_nets_functions, global_vectors, process
     model_scores= run_k_fold(neural_nets_functions, train_document_vecs, labels, epochs, n_folds)
     return model_scores
 
+
 def shuffle_data(X, Y):
     np.random.seed(1337)
     shuffle_indexes = np.arange(X.shape[0])
     np.random.shuffle(shuffle_indexes)
     X = X[shuffle_indexes]
     Y = Y[shuffle_indexes]
-    
+   
     return X, Y
+   
     
 def split_data(X, Y, split=0.8):
     split_size = int(X.shape[0]*split)
     train_x, val_x = X[:split_size], X[split_size:]
     train_y, val_y = Y[:split_size], Y[split_size:]
-    
+   
     return train_x, val_x, train_y, val_y
+
 
 def get_prediction(neural_net, global_vectors, full_corpus, total_training_tweets, nr_pos_tweets,kaggle_name, epochs, split=0.8):
     """
-    Input: 
-    neural_net: Name of a neural net model 
-    global_vectors: global vectors created out the gensim-.txt files. 
+    Input:
+    neural_net: Name of a neural net model
+    global_vectors: global vectors created out the gensim-.txt files.
     total_training_tweets: (int) Number of tweets that are training tweets. Assums that the first poriton of the corpus is
-    training tweets, the second part is the unseen test set. 
+    training tweets, the second part is the unseen test set.
     nr_pos_tweets: (int) number of traning tweets that are positiv
-    kaggle_name: Name for csv file, must end in '.csv'. 
-    
-    Output: 
-    pred_ones: the predicions (1 or -1) 
-    - a .csv file with name 'kaggle_name' 
+    kaggle_name: Name for csv file, must end in '.csv'.
+   
+    Output:
+    pred_ones: the predicions (1 or -1)
+    - a .csv file with name 'kaggle_name'
     """
     num_of_dim = global_vectors.syn0.shape[1]
     # seperate traindata and testdata
-    train_corpus = full_corpus[:total_training_tweets:] 
-    predict_corpus = full_corpus[total_training_tweets::] 
+    train_corpus = full_corpus[:total_training_tweets:]
+    predict_corpus = full_corpus[total_training_tweets::]
     # Build a vector of all the words in a tweet
     train_document_vecs = np.concatenate([buildWordVector(doc, num_of_dim, global_vectors) for doc in train_corpus])
     train_document_vecs = sk.preprocessing.scale(train_document_vecs)
     labels = create_labels(total_training_tweets, nr_pos_tweets)
-    
+   
     train_document_vecs, labels = shuffle_data(train_document_vecs,labels)
     train_x, val_x, train_y, val_y = split_data(train_document_vecs, labels, split)
-    
+   
     test_document_vecs = np.concatenate([buildWordVector(doc, num_of_dim, global_vectors) for doc in predict_corpus])
     test_document_vecs = sk.preprocessing.scale(test_document_vecs)
-    
+   
     model_name = neural_net.__name__
-    
+   
     model = neural_net(num_of_dim)
-    
+   
     early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
     model_checkpoint = keras.callbacks.ModelCheckpoint("best_neural_model_prediction_model.hdf5", monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False, mode='auto')
-
+ 
     history = model.fit(train_x, train_y, epochs=epochs, batch_size=1024, verbose=1, callbacks=[early_stopping, model_checkpoint], validation_data=(val_x, val_y))
-    
+   
     model = load_model('best_neural_model_prediction_model.hdf5')
-
+ 
     pred=model.predict(test_document_vecs)
-    
+   
     pred_ones=[]
     for i in pred:
         if i> 0.5:
             pred_ones.append(1)
         else:
             pred_ones.append(-1)
-            
+           
     #CREATING SUBMISSION
     ids = list(range(1,10000+1))
     HL.create_csv_submission(ids, pred_ones,kaggle_name)
-
+ 
     return pred_ones
+
+
--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 import glove_module as GV
+import pickle
 
 def create_corpus(inputfiles):
     """
@@ -249,7 +250,53 @@ def get_global_vectors(dimension):
     else: 
         print('not valid dimension')
         global_vectors=-1
-    return global_vectors     
+    return global_vectors   
+
+class MacOSFile(object):
+    """
+    This object makes us the objects able to get pickled in Mac event though they superseed the limit of 4GB.
+    """
+
+    def __init__(self, f):
+        self.f = f
+
+    def __getattr__(self, item):
+        return getattr(self.f, item)
+
+    def read(self, n):
+        # print("reading total_bytes=%s" % n, flush=True)
+        if n >= (1 << 31):
+            buffer = bytearray(n)
+            idx = 0
+            while idx < n:
+                batch_size = min(n - idx, 1 << 31 - 1)
+                # print("reading bytes [%s,%s)..." % (idx, idx + batch_size), end="", flush=True)
+                buffer[idx:idx + batch_size] = self.f.read(batch_size)
+                # print("done.", flush=True)
+                idx += batch_size
+            return buffer
+        return self.f.read(n)
+
+    def write(self, buffer):
+        n = len(buffer)
+        print("writing total_bytes=%s..." % n, flush=True)
+        idx = 0
+        while idx < n:
+            batch_size = min(n - idx, 1 << 31 - 1)
+            print("writing bytes [%s, %s)... " % (idx, idx + batch_size), end="", flush=True)
+            self.f.write(buffer[idx:idx + batch_size])
+            print("done.", flush=True)
+            idx += batch_size
+
+
+def pickle_dump(obj, file_path):
+    with open(file_path, "wb") as f:
+        return pickle.dump(obj, MacOSFile(f), protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def pickle_load(file_path):
+    with open(file_path, "rb") as f:
+        return pickle.load(MacOSFile(f))
 
    
             
